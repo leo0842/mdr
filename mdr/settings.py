ec4be6307273b488a901dfc5d143539a16086e32
sungsan <kunga077@gmail.com>
2020-09-16 20:06:58 +0900

    secret key 경로 재수정

--- mdr/settings.py
+++ mdr/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ["INSTA_SECRET_KEY"]
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
